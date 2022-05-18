789a44cf587c4ce6cf253191687ffdd2fd7fe7f4
Felddy <markf@geekpad.com>
2022-05-18 17:34:10 -0400

    Migrate project towards Python library skeleton

---
+++ setup.py
@@ -0,0 +1,96 @@
+"""
+This is the setup module for the starlink-exporter docker project.
+
+Based on:
+
+- https://packaging.python.org/distributing/
+- https://github.com/pypa/sampleproject/blob/master/setup.py
+- https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure
+"""
+
+# Standard Python Libraries
+from glob import glob
+from os.path import basename, splitext
+
+# Third-Party Libraries
+from setuptools import find_packages, setup
+
+
+def readme():
+    """Read in and return the contents of the project's README.md file."""
+    with open("README.md", encoding="utf-8") as f:
+        return f.read()
+
+
+def package_vars(version_file):
+    """Read in and return the variables defined by the version_file."""
+    pkg_vars = {}
+    with open(version_file) as f:
+        exec(f.read(), pkg_vars)  # nosec
+    return pkg_vars
+
+
+setup(
+    name="starlink_exporter",
+    # Versions should comply with PEP440
+    version=package_vars("src/_version.py")["__version__"],
+    description="starlink_exporter python library",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/felddy",
+    # The project's main homepage
+    download_url="https://github.com/felddy/starlink-exporter",
+    # Author details
+    author="Mark Feldhousen",
+    author_email="markf@geekpad.com",
+    license="License :: OSI Approved :: MIT License",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        "Topic :: System :: Networking :: Monitoring",
+    ],
+    python_requires=">=3.6",
+    # What does your project relate to?
+    keywords="starlink prometheus",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    install_requires=[
+        "docopt",
+        "grpcio == 1.46.1",
+        "grpcio-tools == 1.46.1",
+        "schema",
+        "setuptools >= 24.2.0",
+    ],
+    extras_require={
+        "test": [
+            "coverage",
+            # coveralls 1.11.0 added a service number for calls from
+            # GitHub Actions. This caused a regression which resulted in a 422
+            # response from the coveralls API with the message:
+            # Unprocessable Entity for url: https://coveralls.io/api/v1/jobs
+            # 1.11.1 fixed this issue, but to ensure expected behavior we'll pin
+            # to never grab the regression version.
+            "coveralls != 1.11.0",
+            "pre-commit",
+            "pytest-cov",
+            "pytest",
+        ]
+    },
+    # Conveniently allows one to run the CLI tool as `example`
+    entry_points={
+        "console_scripts": [
+            "starlink-exporter = starlink_exporter.starlink_exporter:main"
+        ]
+    },
+)
---
+++ src/starlink_exporter/starlink_exporter.py
@@ -0,0 +1,120 @@
+"""A Prometheus exporter for the Starlink dish.
+
+Connects to a Starlink dish and presents the data as Prometheus metrics.
+
+EXIT STATUS
+    This utility exits with one of the following values:
+    0   Completed successfully.
+    >0  An error occurred.
+
+Usage:
+  starlink-exporter [--log-level=LEVEL] [options]
+  starlink-exporter (-h | --help)
+
+Options:
+  -h --help                   Show this message.
+  -p --port=PORT              Port to listen on [default: 9817].
+  -d --dish-address=ADDRESS   The address of the dish
+                              [default: 192.168.100.1:9200].
+  --log-level=LEVEL           If specified, then the log level will be set to
+                              the specified value.  Valid values are "debug",
+                              "info", "warning", "error", and "critical".
+                              [default: info]
+"""
+
+# Standard Python Libraries
+import logging
+import sys
+from typing import Any, Dict
+
+# Third-Party Libraries
+import docopt
+import grpc
+from schema import And, Schema, SchemaError, Use
+
+# cisagov Libraries
+import spacex.api.device.device_pb2 as pb2
+from spacex.api.device.device_pb2_grpc import DeviceStub
+
+from ._version import __version__
+
+
+def run(listen_port: int, dish_address: str) -> None:
+    """Connect to the dish using gRPC and print out the device info."""
+    # TODO handle connection errors
+    with grpc.insecure_channel(dish_address) as channel:
+        client = DeviceStub(channel)
+
+        status = client.Handle(pb2.Request(get_status={}), timeout=10)
+        # history = client.Handle(pb2.Request(get_history={}), timeout=10)
+        # obstruction_map = client.Handle(
+        #     pb2.Request(dish_get_obstruction_map={}), timeout=10
+        # )
+
+        logging.info(f"Device id: f{status.dish_get_status.device_info.id}")
+        logging.info(
+            f"Software version: f{status.dish_get_status.device_info.software_version}"
+        )
+        logging.info(
+            f"Hardware version: f{status.dish_get_status.device_info.hardware_version}"
+        )
+
+        # Drop into an IPython shell
+        # Third-Party Libraries
+        import IPython
+
+        IPython.embed(colors="neutral")
+
+
+def main() -> None:
+    """Set up logging and call the example function."""
+    args: Dict[str, str] = docopt.docopt(__doc__, version=__version__)
+    # Validate and convert arguments as needed
+    schema: Schema = Schema(
+        {
+            "--dish-address": And(
+                str,
+                lambda n: len(n.split(":")) == 2,
+                error="--dish-address must be of the form <host>:<port>.",
+            ),
+            "--port": And(
+                Use(int),
+                lambda n: n > 0 and n < 65536,
+                error="--port must be an integer between 1 and 65535 inclusive.",
+            ),
+            "--log-level": And(
+                str,
+                Use(str.lower),
+                lambda n: n in ("debug", "info", "warning", "error", "critical"),
+                error="Possible values for --log-level are "
+                + "debug, info, warning, error, and critical.",
+            ),
+            str: object,  # Don't care about other keys, if any
+        }
+    )
+
+    try:
+        validated_args: Dict[str, Any] = schema.validate(args)
+    except SchemaError as err:
+        # Exit because one or more of the arguments were invalid
+        print(err, file=sys.stderr)
+        sys.exit(1)
+
+    # Assign validated arguments to variables
+    dish_address: str = validated_args["--dish-address"]
+    log_level: str = validated_args["--log-level"]
+    port: int = validated_args["--port"]
+
+    # Set up logging
+    logging.basicConfig(
+        format="%(asctime)-15s %(levelname)s %(message)s", level=log_level.upper()
+    )
+
+    logging.debug("Starting Starlink exporter with the following arguments:")
+    logging.debug(f"  dish address: {dish_address}")
+    logging.debug(f"  server port: {port}")
+
+    run(port, dish_address)
+
+    # Stop logging and clean up
+    logging.shutdown()
